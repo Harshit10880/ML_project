96489f4a64804c4099af22015a9c2f8e5ea01db2
Harshit10880 <harshit.cha10@gmail.com>
2026-05-01 22:30:56 +0530

    added new file of python

---
+++ core_file/python.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import StandardScaler
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+
+df = pd.read_csv('retail_practice_data_cleaned.csv')
