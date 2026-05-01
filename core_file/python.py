d1efbc5b42ae0fed63cd0b16af19fe86dd4fb107
Harshit10880 <harshit.cha10@gmail.com>
2026-05-01 22:36:26 +0530

    added till prediction

--- core_file/python.py
+++ core_file/python.py
@@ -47,3 +47,14 @@ print("X head:")
 display(X.head())
 print("\ny head:")
 display(y.head())
+
+
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+model = LogisticRegression()
+model.fit(X_train_scaled, y_train)
