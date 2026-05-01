1034fefecb0ea639323b436b17be574f22e69211
Harshit10880 <harshit.cha10@gmail.com>
2026-05-01 22:35:24 +0530

    splitting data

--- core_file/python.py
+++ core_file/python.py
@@ -33,3 +33,17 @@ df['Product_Category'] = df['Product_Category'].replace('electrnics', 'Electroni
 df = pd.get_dummies(df, columns=['Product_Category'], prefix='Cat', dtype=int)
 
 df['Return_Status'] = df['Return_Status'].map({'Yes': 1, 'No': 0})
+
+
+
+# Visulization differences
+feature_columns = ['Customer_Age', 'Quantity', 'Unit_Price',
+                   'Pay_Cash', 'Pay_Credit Card', 'Pay_Debit Card', 'Pay_E-wallet',
+                   'Cat_Beauty', 'Cat_Clothing', 'Cat_Electronics', 'Cat_Grocery', 'Cat_Home']
+X = df[feature_columns]
+y = df['Return_Status']
+
+print("X head:")
+display(X.head())
+print("\ny head:")
+display(y.head())
